217225a7d3eef571477f5b79eec5a59e457abede
Milton Reyes <milton.kings1904@gmail.com>
2026-02-01 18:35:46 -0600

    Add files via upload

---
+++ 00.5_hello_ejerc.py
@@ -0,0 +1,52 @@
+
+## Ejerciico 1
+print("¡Hola Mundo! ")
+
+# Ejercicio 2
+# este es un comentario de una sola línea
+
+#Ejercicio 3
+print("Soy Milton, tengo 16 años y me gusta programar en Python.")
+
+#Ejercicio 4
+print(type("Hola Mundo"))  # Muestra el tipo de dato de la cadena - string
+print(type(16))
+print(type(1.75))
+
+#Ejercicio 5
+"""
+Los tipos de datos en Python son:
+- str (cadena de texto)
+- int (número entero)
+- float (número decimal)
+- bool (valor booleano: True o False)
+- complex (número complejo)
+- list (lista)
+- tuple (tupla)
+- dict (diccionario)
+- set (conjunto)
+- NoneType (tipo especial que representa la ausencia de valor)
+En Python, todo lo que se escribe entre comillas (simples o dobles) es una cadena de texto (string).
+Los números escritos sin comillas son tratados como enteros (
+
+"""
+
+# Ejercicio 6
+print("Hola" + "mundo") # Concatenación de cadenas
+
+# Ejercicio 7
+nombre = "Milton"
+edad = 16
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años.") # Uso de f-strings para formatear cadenas
+
+# Ejercicio 8
+print("¿Cuál es tu nombre?")
+nombre_usuario = input()  # Captura la entrada del usuario
+print(f"¡Hola,  {nombre_usuario}!")  # Saludo personalizado al usuario
+
+# Ejercicio 9
+print( 1 + 2 )        # Suma
+print(type(1 + 2))  # Muestra el tipo de dato del resultado - int
+
+# Ejercicio 10
+#El programa del ejercicio 9 realiza una suma de dos números enteros (1 y 2) y muestra el resultado (3) en la consola. Además, utiliza la función type() para mostrar el tipo de dato del resultado, que en este caso es un entero (int).
---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+# Hola Mundo!! (de nuevo)
+
+print("Hola, mundo!!") 
+
+"""
+Este es un comentario de varias 
+líneas
+"""
+
+print(type("Hola, mundo!!"))  # Muestra el tipo de dato de la cadena - string
+
+"""
+En conclusión "Hola, mundo!!" es una cadena 
+de texto en Python/String,
+todo texto que se escribe es un string. 
+Ecxepto los números que se escriben sin comillas, los valores 
+enteros, floats, booleanos, etc.
+"""
+
+
+print(type(42))          # Muestra el tipo de dato del número - int     
+print(type(3.14))        # Muestra el tipo de dato del número decimal - float
+print(type(True))        # Muestra el tipo de dato del valor booleano - bool
+print(type(False))       # Muestra el tipo de dato del valor booleano - bool
+print(type(3 + 4j))     # Muestra el tipo de dato del número complejo - complex
